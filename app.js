aa0dc7abbfba04cfddf30a47b6a676d7a9e224e1
Renato Macedo <renatomaceneto@gmail.com>
2020-02-23 19:31:40 -0300

    test 2

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req, res) => {
-	res.send('Creating a vps, setting up ssh, using nginx');
+app.get('/', (req, res) => {
+	res.write('Creating a vps, setting up ssh, using nginx');
+	res.write('\nfrom local environment');
+	res.end()
 });
 
 
