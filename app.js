2d8b8febe42f7ddd195916a38e827db6d76eda98
Renato Macêdo <renatomaceneto@gmail.com>
2020-02-23 21:48:16 +0000

    a test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
-	res.send('Hello from Node');
+	res.send('Creating a vps, setting up ssh, using nginx');
 });
 
 
